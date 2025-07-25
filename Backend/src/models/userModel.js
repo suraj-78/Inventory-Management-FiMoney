17a5c022c3c17020b0a157a8124eee7bb994293b
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-07-25 15:29:55 +0530

    setting up project backend

---
+++ Backend/src/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username : {type: String , unique : true}
+    password : String, 
+    firstName : String, 
+    lastName : String
+    }
+
+);
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = {
+    userModel
+}
+
