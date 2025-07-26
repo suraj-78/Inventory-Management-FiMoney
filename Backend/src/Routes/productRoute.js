0bf8c40d744af25f813032f46a4b9a9f53505754
Suraj <suraj.2201082cs@iiitbh.ac.in>
2025-07-26 06:08:35 +0530

    removed test bug

--- Backend/src/Routes/productRoute.js
+++ Backend/src/Routes/productRoute.js
@@ -7,6 +7,6 @@ const { userAuthmiddleware } = require("../middleware/userAuthVerify");
 
 productRoute.post("/", userAuthmiddleware, productController.addProduct);
 productRoute.get("/", userAuthmiddleware,  productController.getProducts);
-productRoute.post("/:id/quantity",userAuthmiddleware, productController.updateQuantity);
+productRoute.put("/:id/quantity",userAuthmiddleware, productController.updateQuantity);
 
 module.exports = productRoute;
